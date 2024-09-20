c1bc26525b647796b942992be0601d96744bd076
Gustavo <gustavodossantos111@hotmail.com>
2024-09-20 07:44:45 -0300

    Layout pronto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,7 +10,7 @@ export const routes: Routes = [
     },
 
     {
-        path:"cadastrar",
+        path:"cadastro",
         component: CadastroComponent
     }, 
 
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
 import { LayoutLoginComponent } from '../../components/layout-login/layout-login.component';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [LayoutLoginComponent],
+  imports: [
+    LayoutLoginComponent,
+    ReactiveFormsModule
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-
+  loginForm!: FormGroup;
+  
+  constructor(){
+    this.loginForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      senha: new FormControl('', [Validators.required, Validators.minLength(6)])
+    })
+  }
 }
