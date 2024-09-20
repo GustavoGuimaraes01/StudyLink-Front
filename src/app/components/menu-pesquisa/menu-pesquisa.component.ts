0e1963a2129325b113ea02fe074e65fc4ff37d2a
SENAI Zerbini <aluno@escolar509.sp.senai.br>
2024-09-20 17:12:03 -0300

    Menu

---
+++ src/app/components/menu-pesquisa/menu-pesquisa.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-pesquisa',
+  standalone: true,
+  imports: [],
+  templateUrl: './menu-pesquisa.component.html',
+  styleUrl: './menu-pesquisa.component.css'
+})
+export class MenuPesquisaComponent {
+
+}
