d7b217d683c27d7a1b10999b05d1e70795ba8976
Gustavo <gustavodossantos111@hotmail.com>
2024-10-19 08:02:29 -0300

    Agendor com o estilo padrao

--- src/app/components/agendador/agendador.component.ts
+++ src/app/components/agendador/agendador.component.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
 import { ScheduleModule } from '@syncfusion/ej2-angular-schedule';
 import { WeekService, MonthService, WorkWeekService, DayService, AgendaService } from '@syncfusion/ej2-angular-schedule';
+import { registerLicense } from '@syncfusion/ej2-base';
+
+// Registering Syncfusion license key
+registerLicense('Ngo9BigBOggjHTQxAR8/V1NDaF5cWWtCf1JpRGRGfV5ycEVAal5VTnJbUj0eQnxTdEFiWX5XcHJRT2VYV0VwXA==');
 
 @Component({
   selector: 'app-agendador',
