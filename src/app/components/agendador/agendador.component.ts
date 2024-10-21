5f83bc811e738a03c0335fdf19f6784f43b244fc
Gustavo <gustavodossantos111@hotmail.com>
2024-10-21 19:14:00 -0300

    Cabeçalho com os dados necessarios

--- src/app/components/agendador/agendador.component.ts
+++ src/app/components/agendador/agendador.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { DatePicker } from '@syncfusion/ej2-calendars';
 import { ScheduleModule, View } from '@syncfusion/ej2-angular-schedule';
-import { WeekService, MonthService, WorkWeekService, DayService, AgendaService } from '@syncfusion/ej2-angular-schedule';
+import { EventSettingsModel,WeekService, MonthService,  DayService, AgendaService } from '@syncfusion/ej2-angular-schedule';
 import { registerLicense, L10n } from '@syncfusion/ej2-base';
 import { loadCldr } from '@syncfusion/ej2-base';
 import ptNumberData from '@syncfusion/ej2-cldr-data/main/pt/numbers.json';
@@ -9,13 +9,17 @@ import ptTimeZoneData from '@syncfusion/ej2-cldr-data/main/pt/timeZoneNames.json
 import ptGregorian from '@syncfusion/ej2-cldr-data/main/pt/ca-gregorian.json';
 import ptNumberingSystem from '@syncfusion/ej2-cldr-data/supplemental/numberingSystems.json';
 
-// Registro da licença
+//Chave da licença
 registerLicense('Ngo9BigBOggjHTQxAR8/V1NDaF5cWWtCf1JpRGRGfV5ycEVAal5VTnJbUj0eQnxTdEFiWX5XcHJRT2VYV0VwXA==');
 
-// Carregar os dados da cultura brasileira
+// O loadCldr carrega os dados no formato que o Brasil utiliza
 loadCldr(ptNumberData, ptTimeZoneData, ptGregorian, ptNumberingSystem);
+//ptNumberData = formato dos numeros usados no Brasil
+//ptTimeZoneData = fuso horario brasileiro, 
+//ptgregorian = formato de datas, calendario gragoriano 
+//ptNumberSystem = algorismos dos numeros 0-9
 
-// Carregar as traduções para o português (pt-BR)
+//Traduz as informaç~çeos para o portugues
 L10n.load({
   'pt': {
     'schedule': {
@@ -25,20 +29,20 @@ L10n.load({
       'month': 'Mês',
       'agenda': 'Agenda',
       'today': 'Hoje',
-      'noEvents': 'Nenhum evento',
+      'noEvents': 'Nenhum tarefa',
       'allDay': 'Dia inteiro',
       'start': 'Início',
       'end': 'Fim',
-      'more': 'mais',
       'saveButton': 'Salvar',
       'cancelButton': 'Cancelar',
       'deleteButton': 'Excluir',
-      'newEvent': 'Novo evento',
-      'editEvent': 'Editar evento',
-      'deleteEvent': 'Excluir evento',
+      'newEvent': 'Novo tarefa',
+      'editEvent': 'Editar tarefa',
+      'title':'titulo',
+      'deleteEvent': 'Excluir tarefa',
       'confirmation': 'Confirmação',
-      'deleteContent': 'Tem certeza de que deseja excluir este evento?',
-      'deleteMultipleContent': 'Tem certeza de que deseja excluir esses eventos?',
+      'deleteContent': 'Tem certeza de que deseja excluir este tarefa?',
+      'deleteMultipleContent': 'Tem certeza de que deseja excluir essa tarefa?',
       'days': {
         'sunday': 'Domingo',
         'monday': 'Segunda-feira',
@@ -52,23 +56,23 @@ L10n.load({
   }
 });
 let datepickerObject: DatePicker = new DatePicker({
-  // sets the locale property.
+  //Injeta as propriedades locais .
   locale: 'pt',
   value: new Date()
 });
 datepickerObject.appendTo('#agendador');
-
 @Component({
   selector: 'app-agendador',
   standalone: true,
   imports: [ScheduleModule],
-  providers: [DayService, WeekService, WorkWeekService, MonthService, AgendaService],
+  providers: [DayService, WeekService, MonthService, AgendaService],
   template: `
     <ejs-schedule 
       height="100%" 
-      width="80%" 
+      width="80%"
       [selectedDate]="dataAtual" 
       locale="pt"
+      [views]="view"
       class="agendador">
     </ejs-schedule>
   `,
@@ -76,4 +80,7 @@ datepickerObject.appendTo('#agendador');
 })
 export class AgendadorComponent {
   public dataAtual: Date = new Date();
+
+  //Ajusta oq sera mostrado no header do calendario
+  public view: View[] = ['Day','Week','Month','Agenda'] 
 }
