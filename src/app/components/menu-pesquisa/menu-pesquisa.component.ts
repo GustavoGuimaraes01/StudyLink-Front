9ab1233b099c458813025e50e2a857c24c48ad1d
Gustavo <gustavodossantos111@hotmail.com>
2024-10-23 04:20:31 -0300

    Calendario fora de posição na tela de home

--- src/app/components/menu-pesquisa/menu-pesquisa.component.ts
+++ src/app/components/menu-pesquisa/menu-pesquisa.component.ts
@@ -1,23 +1,32 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import{MatSidenavModule} from  '@angular/material/sidenav' ;
+import { Component, OnInit } from '@angular/core';
+import { Router, RouterModule } from '@angular/router'; // Importando Router
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
 import { AgendadorComponent } from "../agendador/agendador.component";
-
-
-
+import { NavigationEnd } from '@angular/router'; // Importando NavigationEnd
+import { filter } from 'rxjs/operators'; // Importando filter
 
 @Component({
   selector: 'app-menu-pesquisa',
   standalone: true,
   imports: [RouterModule, MatIconModule, MatButtonModule, MatToolbarModule, MatSidenavModule, MatListModule, AgendadorComponent],
   templateUrl: './menu-pesquisa.component.html',
-  styleUrl: './menu-pesquisa.component.css'
+  styleUrls: ['./menu-pesquisa.component.css'] // Corrigido para styleUrls
 })
+export class MenuPesquisaComponent implements OnInit {
+  currentRoute: string = '';
 
-export class MenuPesquisaComponent {
+  constructor(private router: Router) {}
 
+  ngOnInit() {
+    // Escutar eventos de navegação
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe((event: NavigationEnd) => {
+      this.currentRoute = event.url; // Atualiza a rota atual
+    });
+  }
 }
