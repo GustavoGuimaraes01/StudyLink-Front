eeb8f2110e125d678fb10aea5cc53819d4d937f3
Gustavo <gustavodossantos111@hotmail.com>
2024-10-29 20:48:48 -0300

    Janela para criar os materiais pronta

--- src/app/pages/criar-material/criar-material.component.ts
+++ src/app/pages/criar-material/criar-material.component.ts
@@ -16,5 +16,22 @@ export class CriarMaterialComponent {
     this.isOpen = false;
     this.isOpenChange.emit(this.isOpen); // Atualiza o estado no componente pai
   }
+
+  selectedImage: string | ArrayBuffer | null = null;
+
+  onFileSelected(event: Event): void {
+    const file = (event.target as HTMLInputElement).files?.[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = () => {
+        this.selectedImage = reader.result;
+      };
+      reader.readAsDataURL(file);
+    }
+  }
+
+  removeImage(): void {
+    this.selectedImage = null;
+  }
 }
 
