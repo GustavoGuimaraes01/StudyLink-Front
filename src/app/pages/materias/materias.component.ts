65166f1c1f7d2a198a1f2fe38204de87a5250cb3
Gustavo <gustavodossantos111@hotmail.com>
2024-10-29 22:54:51 -0300

    Mudando os links de edit e delete para buttons

--- src/app/pages/materias/materias.component.ts
+++ src/app/pages/materias/materias.component.ts
@@ -11,6 +11,7 @@ import { CommonModule } from '@angular/common';
   styleUrl: './materias.component.css',
 })
 export class MateriasComponent {
+  selectedMaterialId: number | null = null;
 
   
   materiais = [
